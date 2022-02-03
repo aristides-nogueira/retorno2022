f1aa97df6992a3b2493314cc2859024fcf3a7277
Rod <rodfeal@gmail.com>
2022-02-03 15:30:23 -0300

    Updates

---
+++ precache-manifest.e93b9cf807737cf6a19d428361ee8cff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70feb4886c031e7f429c62a364f3e1a4",
+    "url": "/retorno2022/index.html"
+  },
+  {
+    "revision": "fe06fdc06638f145cac7",
+    "url": "/retorno2022/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d21c26e6ce3d308e0efc",
+    "url": "/retorno2022/static/js/2.957054e6.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/retorno2022/static/js/2.957054e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe06fdc06638f145cac7",
+    "url": "/retorno2022/static/js/main.e0df9d25.chunk.js"
+  },
+  {
+    "revision": "ac14c49b6234aeeb4a40",
+    "url": "/retorno2022/static/js/runtime-main.65f5de51.js"
+  }
+]);
